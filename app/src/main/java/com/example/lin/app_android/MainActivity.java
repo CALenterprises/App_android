1e59d9e7aa1f8992973683f76eb705ffcb824897
Alvin Lin <omgimanerd@yahoo.com>
2014-07-27 12:28:57 -0400

    Fake listview weather forecast

--- app/src/main/java/com/example/lin/app_android/MainActivity.java
+++ app/src/main/java/com/example/lin/app_android/MainActivity.java
@@ -10,7 +10,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -51,13 +55,30 @@ public class MainActivity extends ActionBarActivity {
      */
     public static class PlaceholderFragment extends Fragment {
 
+        private ArrayAdapter<String> adapter;
+
         public PlaceholderFragment() {
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
+                                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            ArrayList forecast = new ArrayList();
+            forecast.add("Today - Sunny - 88/63");
+            forecast.add("Tomorrow - Foggy - 70/46");
+            forecast.add("Weds - Cloudy - 72/63");
+            forecast.add("Thurs - Rainy - 64/51");
+            forecast.add("Fri - Foggy - 70/46");
+            forecast.add("Sat - Sunny - 76/68");
+
+            adapter = new ArrayAdapter<String>(getActivity(),
+                                               R.layout.list_item_forecast,
+                                               R.id.list_item_forecast_textview,
+                                               forecast);
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(adapter);
             return rootView;
         }
     }
